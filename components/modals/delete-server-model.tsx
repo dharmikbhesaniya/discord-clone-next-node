5c9fdc8a67977f3a8d4f9e923fc86383173c4f2d
dharmik bhesaniya <dsbhesaniya4811@gmail.com>
2024-01-28 12:24:53 +0530

    feature: delete server

--- components/modals/delete-server-model.tsx
+++ components/modals/delete-server-model.tsx
@@ -14,20 +14,19 @@ import { Button } from "../ui/button";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
-export const LeaveServerModal = () => {
+export const DeleteServerModal = () => {
   const [isLoading, setIsLoading] = useState(false);
   const { isOpen, onOpen, onClose, type, data } = useModal();
   const router = useRouter();
 
   const { server } = data;
-  const isModalOpen = isOpen && type == "leaveServer";
+  const isModalOpen = isOpen && type == "deleteServer";
 
   const onClick = async () => {
     try {
       setIsLoading(true);
-      await axios.patch(`/api/servers/${server?.id}/leave`);
+      await axios.delete(`/api/servers/${server?.id}`);
       onClose();
-      
       router.refresh();
       router.push("/");
     } catch (error) {
@@ -42,14 +41,14 @@ export const LeaveServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Leave Servers
+            Delete Servers
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
-            Are you sure want to leave
+            Are you sure want to do this? <br />
             <span className="font-semibold text-indigo-500">
               {server?.name}
-            </span>
-            ?
+            </span>{" "}
+            will be permanently delete.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter className="bg-gray-100 px-6 py-4">
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,7 @@ import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-model";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { LeaveServerModal } from "../modals/leave-server-model";
+import { DeleteServerModal } from "../modals/delete-server-model";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -25,6 +26,7 @@ export const ModalProvider = () => {
       <MembersModal />
       <CreateChannelModal />
       <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
