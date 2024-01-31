775ea1b9677fe005e32bfa560d322701835ed36b
dharmik bhesaniya <dsbhesaniya4811@gmail.com>
2024-01-31 22:12:49 +0530

    feature: connect socket.io

--- types.ts
+++ types.ts
@@ -1,4 +1,7 @@
 import { Channel, Member, Profile, Server } from "@prisma/client";
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIoServer } from "socket.io";
 
 export type ServerWithMembersWithProfiles = Server & {
   members: Member &
@@ -9,4 +12,12 @@ export type ServerWithMembersWithProfiles = Server & {
       profile: Profile;
     }[];
   channels: Channel[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIoServer;
+    };
+  };
 };
