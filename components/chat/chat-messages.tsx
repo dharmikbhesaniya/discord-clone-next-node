c11b2c3231021cee68d4ba7243b761f5fc941f14
dharmik bhesaniya <dsbhesaniya4811@gmail.com>
2024-02-08 11:08:29 +0530

    feature: audio and video call

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -89,10 +89,10 @@ export const ChatMessages = ({
   return (
     <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto ">
       {/* {!hasNextPage && (
-        <div>
+        <>
           <div className="flex-1" />
           <ChatWelcome type={type} name={name} />
-        </div>
+        </>
       )} */}
       {!hasNextPage && <div className="flex-1" />}
       {!hasNextPage && (
